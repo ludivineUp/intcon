c965dc9db63cbe15b7e3ef936dd559cfa010fc01
Ludivine <lcrepin@ludivinecrepin.fr>
2020-11-13 14:03:39 +0100

    Update IntegrationTest.java

--- GeekQuote/app/src/test/java/com/devforxkill/geekquote/IntegrationTest.java
+++ GeekQuote/app/src/test/java/com/devforxkill/geekquote/IntegrationTest.java
@@ -2,6 +2,7 @@ package com.devforxkill.geekquote;
 
 import android.content.Context;
 import android.test.mock.MockContext;
+import android.util.Log;
 
 import androidx.test.InstrumentationRegistry;
 import androidx.test.core.app.ApplicationProvider;
@@ -9,6 +10,7 @@ import androidx.test.core.app.ApplicationProvider;
 import com.devforxkill.geekquote.dao.DaoFactory;
 import com.devforxkill.geekquote.model.Quote;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -33,5 +35,21 @@ public class IntegrationTest {
     @Test
     public void getAll_test(){
         assertNotNull(DaoFactory.getQuoteDao(context).getQuotes());
+    }
+
+    @Test
+    public void add_test(){
+        Quote q = new Quote(0, "toto", 5, LocalDate.now());
+        assertTrue(DaoFactory.getQuoteDao(context).addQuote(q) > 0);
+        // test auto increment
+        int size = DaoFactory.getQuoteDao(context).getQuotes().size();
+        assertTrue(q.getId() == size);
+    }
+
+    @Test
+    public void getbyid_test(){
+        Quote q = DaoFactory.getQuoteDao(context).getByID(1);
+        Log.d("Test", q.toString());
+        assertNotNull(q);
     }
 }
